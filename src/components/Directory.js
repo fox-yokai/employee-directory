91abbd3106a9c4ca53e533100619b5c73d8ee328
fox-yokai <pat.w.cain@gmail.com>
2020-05-01 10:00:36 -0500

    rendering rows from the api

--- src/components/Directory.js
+++ src/components/Directory.js
@@ -1,30 +1,47 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import API from "../utils/API";
+import Container from "./Container";
 
-class Directory extends Component {
-    state = {
-        result: {},
+
+
+const Directory = () => {
+    const [directory, setDirectory] = useState({
+        results: [],
         search: ""
-    };
+    });
 
-    componentDidMount() {
+
+    useEffect(() => {
         API.getEmployees()
-        .then(res => this.setState({ result: res.data.results }))
+        .then(res => setDirectory({ results: res.data.results }))
         .catch(err => console.log(err));
-    };
+    }, []);
 
-  
-
-    // handleInputChange = event => {
-    //     const 
-    // }
-
-    render() {
-        return (
-        <div>I am the directory</div>
+    return (
+        <div>
+            <Container>
+            {directory.results.map(result => (
+                <div className="row" key={result.id.value}>
+                    <div className="col-sm thumbnail">
+                        <img src={result.picture.thumbnail} alt={result.name.first}></img>
+                    </div>
+                    <div className="col-sm name">
+                        {result.name.first} {result.name.last}
+                    </div>
+                    <div className="col-sm phone">
+                        {result.phone}
+                    </div>
+                    <div className="col-sm email">
+                        {result.email}
+                    </div>
+                    <div className="col-sm dob">
+                        {result.dob.date.substring(0, 10)}
+                    </div>
+                </div>
+            ))}
+            </Container>
+        </div>
         )
-    }
-}
-
+};
 
 export default Directory;
